694157babcb8589fcc56fc7875271f9ed242a335
James Chen <chun-pengc.chen@wsu.edu>
2019-10-29 14:32:33 -0700

    update git page

--- page/render_book.r
+++ page/render_book.r
@@ -1,4 +1,4 @@
 # system("rm _main.Rmd")
 library(data.table)
 library(magrittr)
-bookdown::render_book("index.Rmd", output_fotmat = bookdown::gitbook(split_by=c("chapter")))
+bookdown::render_book("index.Rmd", output_format = bookdown::gitbook(split_by=c("chapter")))
